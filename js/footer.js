55fc9ec6685323cbc30c110bd560a216b25fc425
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2026-03-10 19:42:49 +0000

    adsense compliance fixes

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+    document.documentElement.style.setProperty(
+        '--scrollbar-width',
+        (window.innerWidth - document.documentElement.clientWidth) + 'px'
+    );
+
     const privacy_modal = document.querySelector('.privacy__modal');
     const privacy_links = document.querySelectorAll('.privacy-link');
 
@@ -17,5 +22,47 @@ document.addEventListener("DOMContentLoaded", function() {
     const privacy_close = document.querySelector('.privacy__close');
     privacy_close.addEventListener("click", () => {
         privacy_modal.close();
+    });
+
+    // Terms of Service modal
+    const terms_modal = document.querySelector('.terms__modal');
+    const terms_links = document.querySelectorAll('.terms-link');
+
+    terms_modal.addEventListener('click', function (e) {
+        if (e.target === terms_modal) {
+            terms_modal.close();
+        }
+    });
+
+    terms_links.forEach(link => {
+        link.addEventListener("click", () => {
+            terms_modal.showModal();
+        });
+    });
+
+    const terms_close = document.querySelector('.terms__close');
+    terms_close.addEventListener("click", () => {
+        terms_modal.close();
+    });
+
+    // About modal
+    const about_modal = document.querySelector('.about__modal');
+    const about_links = document.querySelectorAll('.about-link');
+
+    about_modal.addEventListener('click', function (e) {
+        if (e.target === about_modal) {
+            about_modal.close();
+        }
+    });
+
+    about_links.forEach(link => {
+        link.addEventListener("click", () => {
+            about_modal.showModal();
+        });
+    });
+
+    const about_close = document.querySelector('.about__close');
+    about_close.addEventListener("click", () => {
+        about_modal.close();
     });
 });
